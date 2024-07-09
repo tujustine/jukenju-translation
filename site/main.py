85244c95b9e7e1dcdfb40ea6f0402ad1cb0f7561
Justine Tu <justine.tu@hotmail.fr>
2024-07-10 00:55:29 +0200

    Procfile

--- site/main.py
+++ site/main.py
@@ -162,4 +162,4 @@ async def recup_message(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
